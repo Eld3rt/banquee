3ce37425d4e166800ac0eb12161222cc5c7fd903
Eld3rt <kirrey8ynwa@gmail.com>
2023-01-07 12:02:46 +0300

    Created section components and added to index

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
-export default defineConfig({
-  // your configuration options here...
-  // https://docs.astro.build/en/reference/configuration-reference/
+export default defineConfig( {
+	integrations: [react()],
 })
