209c6a50eca034e2246b3d47393c94271706b774
Eld3rt <kirrey8ynwa@gmail.com>
2023-01-13 16:06:21 +0300

    Make responsive header

---
+++ src/utilities/toggleHeaderMenu.js
@@ -0,0 +1,16 @@
+export default function toggleHeaderMenu(mobileMenuClassName, navigationClassName, menuHamburgerClassName, menuXClassName) {
+
+	const mobileMenu = document.querySelector(mobileMenuClassName);
+	const navigation = document.querySelector(navigationClassName);
+	const menuHamburger = document.querySelector(menuHamburgerClassName);
+	const menuX = document.querySelector(menuXClassName);
+
+	mobileMenu.addEventListener('click', () => {
+		navigation.classList.contains('df') ?
+		mobileMenu.setAttribute('aria-expanded', false) :
+		mobileMenu.setAttribute('aria-expanded', true);
+		navigation.classList.toggle('df');
+		menuHamburger.classList.toggle('db');
+		menuX.classList.toggle('db');
+	});
+}
