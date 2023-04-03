21d53da903c62c36dc85a8c72a0749808f293c56
Eld3rt <kirrey8ynwa@gmail.com>
2023-04-03 15:08:01 +0300

    Use Accounts as object in Header Component

---
+++ src/data/accounts.js
@@ -0,0 +1,20 @@
+export default [
+	{
+		title: 'Basic',
+		description:'Lorem ipsum dolor',
+		imageURL: 'assets/images/card-basic.png',
+		popular: true,
+	},
+	{
+		title: 'Premium',
+		description:'Lorem ipsum dolor',
+		imageURL: 'assets/images/card-premium.png',
+		popular: false,
+	},
+	{
+		title: 'Gold',
+		description:'Lorem ipsum dolor',
+		imageURL: 'assets/images/card-gold.png',
+		popular: false,
+	}
+]
