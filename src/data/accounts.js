bc77a384f9416f57acaf3dfbb9dc22dc09f39aa7
Eld3rt <kirrey8ynwa@gmail.com>
2023-04-09 18:04:16 +0300

    Complete Features HTML & realisze component scripts

--- src/data/accounts.js
+++ src/data/accounts.js
@@ -1,21 +1,33 @@
 export default [
 	{
 		title: 'Basic',
-		description:'Lorem ipsum dolor',
+		price: 'Free',
+		description: {
+			short: 'Lorem ipsum dolor',
+			extended: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		},
 		imageURL: 'assets/images/card-basic.png',
 		popular: true,
 		visible: true,
 	},
 	{
 		title: 'Premium',
-		description:'Lorem ipsum dolor',
+		price: '$5',
+		description: {
+			short: 'Lorem ipsum dolor',
+			extended: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		},
 		imageURL: 'assets/images/card-premium.png',
 		popular: false,
 		visible: true,
 	},
 	{
 		title: 'Gold',
-		description:'Lorem ipsum dolor',
+		price: '$10',
+		description: {
+			short: 'Lorem ipsum dolor',
+			extended: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+		},
 		imageURL: 'assets/images/card-gold.png',
 		popular: false,
 		visible: true,
