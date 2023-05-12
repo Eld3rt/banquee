6845b0d5d3fbf27a240985db99b9e05d36e87daf
Eld3rt <kirrey8ynwa@gmail.com>
2023-05-12 15:06:06 +0300

    Full project file restructure

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -5,6 +5,7 @@ export default [
 		p1: 'Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.',
 		author: 'Cody Fisher',
 		profession: 'Medical Assistant',
+		active: true
 	},
 	{
 		rating: 5,
@@ -12,34 +13,39 @@ export default [
 		p1: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dui accumsan sit amet nulla facilisi morbi.',
 		p2: 'In nibh mauris cursus mattis. At lectus urna duis convallis convallis tellus. Enim blandit volutpat maecenas volutpat.',
 		author: 'Jenny Wilson',
-		profession: 'Nursing Assistant'
+		profession: 'Nursing Assistant',
+		active: true
 	},
 	{
 		rating: 5,
 		title: 'Elit aute irure tempor cupidatat incididunt',
 		p1: 'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.',
 		author: 'Guy Hawkins',
-		profession: 'President of Sales'
+		profession: 'President of Sales',
+		active: true
 	},
 	{
 		rating: 5,
 		title: 'Sunt qui esse pariatur duis deserunt mollit',
 		p1: 'Aliqua id fugiat nostrud irure ex duis ea quis id quis ad et. Sunt qui esse pariatur duis deserunt mollit dolore cillum minim tempor enim. Elit aute irure tempor cupidatat incididunt sint deserunt ut voluptate aute id deserunt nisi.	',
 		author: 'Robert Fisher',
-		profession: 'Medical Assistant'
+		profession: 'Medical Assistant',
+		active: true
 	},
 	{
 		rating: 5,
 		title: 'Donec et fringilla neque',
 		p1: 'Etiam accumsan porta neque in viverra. Proin eleifend, eros in tristique hendrerit, nisi purus cursus sapien, id ultrices nunc tellus a ipsum. Donec et fringilla neque. Aenean consequat purus quis lectus maximus fermentum.',
 		author: 'Darlene Robertson',
-		profession: 'Dog Trainer'
+		profession: 'Dog Trainer',
+		active: true
 	},
 	{
 		rating: 5,
 		title: 'Etiam accumsan porta neque eros',
 		p1: 'Nulla Lorem mollit cupidatat irure. Laborum magna nulla duis ullamco cillum dolor. Voluptate exercitation incididunt aliquip deserunt reprehenderit elit laborum.',
 		author: 'Dianne Russell',
-		profession: 'Medical Assistant'
+		profession: 'Medical Assistant',
+		active: true
 	},
 ]
